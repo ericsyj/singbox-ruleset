a7d266c6c0c0aaeceb5aae73cdb8ae10e8213644
ericsyj <97294040+ericsyj@users.noreply.github.com>
2024-03-09 19:21:14 +0800

    Update build_ads.sh

--- scripts/build_ads.sh
+++ scripts/build_ads.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
 
+if [ -f ads.json ]; then > ads.json; fi
+
+input=$(curl -fsSL https://raw.githubusercontent.com/Cats-Team/AdRules/main/adrules_domainset.txt)
+
+# 转换为JSON格式
+echo '{
+    "version": 1,
+    "rules": [
+        {
+            "domain_suffix": [' > ads.json
+
+# 遍历每一行
+while IFS= read -r line
+do
+    # 忽略注释行
+    if [[ $line == \#* ]]; then
+        continue
+    fi
+    # 去掉开头的加号
+    line=${line#+}
+    # 添加到JSON文件
+    echo '                "'$line'",' >> ads.json
+done <<< $input
+
+# 移除最后一个逗号
+sed -i '$ s/,$//' ads.json
+
+# 添加结束标记
+echo '            ]
+        }
+    ]
+}' >> ads.json
