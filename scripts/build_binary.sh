43cefdcd527c54d108ce39d0a584552ece0be612
ericsyj <97294040+ericsyj@users.noreply.github.com>
2024-08-11 20:52:04 +0800

    Update build_binary.sh

--- scripts/build_binary.sh
+++ scripts/build_binary.sh
@@ -53,7 +53,8 @@ echo $SINGBOX_VER_PRE >version.txt
 
 RUN_ID=$(curl -s https://api.github.com/repos/PuerNya/sing-box/actions/runs | jq -r '.workflow_runs[] | select(.name == "Build SFA") | .id' | head -n 1)
 ARTIFACT_ID=$(curl -s https://api.github.com/repos/PuerNya/sing-box/actions/runs/$RUN_ID/artifacts | jq '.artifacts[] | .id' | head -n 1)
-curl -L -H "Authorization: token $TOKEN" -o singboxp.zip https://api.github.com/repos/PuerNya/sing-box/actions/artifacts/$ARTIFACT_ID/zip
-unzip singboxp.zip
-rm -f singboxp.zip
+curl -L -H "Authorization: token $TOKEN" https://api.github.com/repos/PuerNya/sing-box/actions/artifacts/$ARTIFACT_ID/zip
+echo $(ls sfa*.zip) | awk -F'-' '{print $2 "-" $3}' >version-p.txt
+unzip sfa*.zip
+rm -f sfa*.zip
 mv SFA-*-foss-arm64-v8a-signed.apk singboxp-armv8.apk
