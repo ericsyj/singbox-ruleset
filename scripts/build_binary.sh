001c6fe7fde0a9bf50c83b538a1a46d6b285e169
ericsyj <674753176@qq.com>
2024-08-13 23:31:55 +0800

    新增同步sing-box-puer

--- scripts/build_binary.sh
+++ scripts/build_binary.sh
@@ -51,11 +51,41 @@ cd sing-box
 tar -czvf CrashCore.tar.gz CrashCore
 echo $SINGBOX_VER_PRE >version.txt
 
+# RUN_ID=$(curl -s https://api.github.com/repos/PuerNya/sing-box/actions/runs | jq -r '.workflow_runs[] | select(.name == "Build SFA") | .id' | head -n 1)
+# ARTIFACT_ID=$(curl -s https://api.github.com/repos/PuerNya/sing-box/actions/runs/$RUN_ID/artifacts | jq '.artifacts[] | .id' | head -n 1)
+# curl -L -H "Authorization: token $TOKEN" -o singbox.zip https://api.github.com/repos/PuerNya/sing-box/actions/artifacts/$ARTIFACT_ID/zip
+# NAME=$(curl -s https://api.github.com/repos/PuerNya/sing-box/actions/runs/$RUN_ID/artifacts | jq '.artifacts[] | .name' | head -n 1)
+# echo $NAME | awk -F'-' '{print $2 "-" $3}' >version-p.txt
+# unzip singbox.zip
+# rm -f singbox.zip
+# mv -f SFA-*-foss-arm64-v8a-signed.apk sing-box-puer
+mkdir tmp
+mkdir sfa-puer
+mkdir sbc-puer
+cd tmp
 RUN_ID=$(curl -s https://api.github.com/repos/PuerNya/sing-box/actions/runs | jq -r '.workflow_runs[] | select(.name == "Build SFA") | .id' | head -n 1)
-ARTIFACT_ID=$(curl -s https://api.github.com/repos/PuerNya/sing-box/actions/runs/$RUN_ID/artifacts | jq '.artifacts[] | .id' | head -n 1)
-curl -L -H "Authorization: token $TOKEN" -o singbox.zip https://api.github.com/repos/PuerNya/sing-box/actions/artifacts/$ARTIFACT_ID/zip
+ARTIFACT_ID_LIST=$(curl -s https://api.github.com/repos/PuerNya/sing-box/actions/runs/$RUN_ID/artifacts | jq '.artifacts[] | .id')
+for ARTIFACT_ID in $ARTIFACT_ID_LIST; do
+    curl -L -H "Authorization: token $TOKEN" -o $ARTIFACT_ID.zip https://api.github.com/repos/PuerNya/sing-box/actions/artifacts/$ARTIFACT_ID/zip
+    unzip -o $ARTIFACT_ID.zip
+    rm -f $ARTIFACT_ID.zip
+    filename=$(curl -s https://api.github.com/repos/PuerNya/sing-box/actions/artifacts/$ARTIFACT_ID | jq -r '.name' | cut -d '-' -f 4-)
+    mv -f *.apk ../sfa-puer/sfa-puer-$filename.apk
+done
+
 NAME=$(curl -s https://api.github.com/repos/PuerNya/sing-box/actions/runs/$RUN_ID/artifacts | jq '.artifacts[] | .name' | head -n 1)
-echo $NAME | awk -F'-' '{print $2 "-" $3}' >version-p.txt
-unzip singbox.zip
-rm -f singbox.zip
-mv SFA-*-foss-arm64-v8a-signed.apk singboxp-armv8.apk
+echo $NAME | awk -F'-' '{print $2 "-" $3}' >../version-p.txt
+
+RUN_ID=$(curl -s https://api.github.com/repos/PuerNya/sing-box/actions/runs | jq -r '.workflow_runs[] | select(.name == "Build Cores") | .id' | head -n 1)
+ARTIFACT_ID_LIST=$(curl -s https://api.github.com/repos/PuerNya/sing-box/actions/runs/$RUN_ID/artifacts | jq '.artifacts[] | .id')
+for ARTIFACT_ID in $ARTIFACT_ID_LIST; do
+    curl -L -H "Authorization: token $TOKEN" -o $ARTIFACT_ID.zip https://api.github.com/repos/PuerNya/sing-box/actions/artifacts/$ARTIFACT_ID/zip
+    unzip -o $ARTIFACT_ID.zip
+    rm -f $ARTIFACT_ID.zip
+    filename=$(curl -s https://api.github.com/repos/PuerNya/sing-box/actions/artifacts/$ARTIFACT_ID | jq -r '.name' | cut -d '-' -f 5-)
+    mkdir -p ../sbc-puer/sbc-puer-$filename
+    mv -f * ../sbc-puer/sbc-puer-$filename
+done
+
+cd ..
+rm -rf tmp
