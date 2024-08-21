3e579de32fbe7ee4f3f8c4a3c0adaa07253a2627
ericsyj <674753176@qq.com>
2024-08-21 19:44:56 +0800

    修改

--- scripts/build_antiad.sh
+++ scripts/build_antiad.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 input=$(curl -fsSL https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-clash.yaml)
 
@@ -33,8 +33,6 @@ echo '            ]
 
 curl -Lo sing-box.tar.gz "https://github.com/SagerNet/sing-box/releases/download/v1.9.4/sing-box-1.9.4-linux-amd64v3.tar.gz"
 tar -zxvf sing-box.tar.gz
-rm sing-box.tar.gz
-chmod +x sing-box-1.9.4-linux-amd64/sing-box
-chmod +x sing-box
-sing-box-1.9.4-linux-amd64/sing-box rule-set compile --output antiad.srs antiad.json
-rm -rf sing-box-1.9.4-linux-amd64
+chmod +x sing-box-1.9.4-linux-amd64v3/sing-box
+sing-box-1.9.4-linux-amd64v3/sing-box rule-set compile --output antiad.srs antiad.json
+rm -rf sing-box-1.9.4-linux-amd64v3 sing-box.tar.gz
