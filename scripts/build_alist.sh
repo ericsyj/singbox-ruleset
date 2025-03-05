6265515216ce64b0d56acda4839381c3753d20ba
ericsyj <97294040+ericsyj@users.noreply.github.com>
2025-03-05 20:37:00 +0800

    Update build_alist.sh

--- scripts/build_alist.sh
+++ scripts/build_alist.sh
@@ -7,7 +7,7 @@ ALIST_VER=$(curl -s https://api.github.com/repos/alist-org/alist/releases/latest
     sed 's/\"//g;s/\,//g;s/\ //g;s/v//' |
     head -n 1)
 
-curl -Lo alist-win64.zip "https://github.com/alist-org/alist/releases/download/v$ALIST_VER/alist-windows-amd64-upx.zip"
+curl -Lo alist-win64.zip "https://github.com/AlistGo/alist/releases/download/v$ALIST_VER/alist-windows-amd64-upx.zip"
 unzip alist-win64.zip
 rm alist-win64.zip
 cp -f alist.exe alist/alist-win64.exe
