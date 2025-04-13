ec23cf2050efca7b1809f8f8cf0b7d32dea3abc8
ericsyj <ericsyj00@gmail.com>
2025-04-13 12:56:15 +0800

    Update data files

---
+++ scripts/build_alist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#获取最新版本openlist
+ALIST_VER=$(curl -s https://api.github.com/repos/OpenListTeam/OpenList/releases/latest |
+    grep tag_name |
+    cut -d ":" -f2 |
+    sed 's/\"//g;s/\,//g;s/\ //g;s/v//' |
+    head -n 1)
+
+curl -Lo openlist-win64.zip "https://github.com/OpenListTeam/OpenList/releases/download/v$ALIST_VER/openlist-windows-amd64-upx.zip"
+unzip openlist-win64.zip
+rm openlist-win64.zip
+mv -f openlist.exe alist/alist-win64.exe
